64fed5aef9cf8b0eae0b7c286991f86e05dc6adc
Matthew Laufer <matthew.j.laufer@gmail.com>
2023-11-19 11:47:32 -0800

    update lualine theme

--- lua/lualine/themes/flashy.lua
+++ lua/lualine/themes/flashy.lua
@@ -2,18 +2,21 @@ local c = require('flashy.colors')
 
 return {
     normal = {
-        a = {bg = c.blue, fg = c.bg1, gui = 'bold'},
-        b = {bg = c.bg1, fg = c.blue},
-        c = {bg = c.bg2, fg = c.fg2},
+        a = { bg = c.blue, fg = c.bg1, gui = 'bold' },
+        b = { bg = c.bg3, fg = c.blue },
+        c = { bg = c.bg2, fg = c.fg2 },
     },
-    insert = {a = {bg = c.orange, fg = c.bg1, gui = 'bold'}, b = {bg = c.bg1, fg = c.orange}},
-    visual = {a = {bg = c.magenta, fg = c.bg1, gui = 'bold'}, b = {bg = c.bg1, fg = c.magenta}},
-    command = {a = {bg = c.cyan, fg = c.bg1, gui = 'bold'}, b = {bg = c.bg1, fg = c.cyan}},
-    replace = {a = {bg = c.purple, fg = c.bg1, gui = 'bold'}, b = {bg = c.bg1, fg = c.purple}},
-    terminal = {a = {bg = c.green, fg = c.bg1, gui = 'bold'}, b = {bg = c.bg1, fg = c.green}},
+    insert = { a = { bg = c.orange, fg = c.bg1, gui = 'bold' }, b = { bg = c.bg3, fg = c.orange } },
+    visual = {
+        a = { bg = c.magenta, fg = c.bg1, gui = 'bold' },
+        b = { bg = c.bg3, fg = c.magenta },
+    },
+    command = { a = { bg = c.cyan, fg = c.bg1, gui = 'bold' }, b = { bg = c.bg3, fg = c.cyan } },
+    replace = { a = { bg = c.purple, fg = c.bg1, gui = 'bold' }, b = { bg = c.bg3, fg = c.purple } },
+    terminal = { a = { bg = c.green, fg = c.bg1, gui = 'bold' }, b = { bg = c.bg3, fg = c.green } },
     inactive = {
-        a = {bg = c.bg2, fg = c.fg3},
-        b = {bg = c.bg1, fg = c.fg3},
-        c = {bg = c.bg2, fg = c.fg2},
+        a = { bg = c.bg2, fg = c.fg3 },
+        b = { bg = c.bg3, fg = c.fg3 },
+        c = { bg = c.bg2, fg = c.fg2 },
     },
 }
